e006c900484ebb2ef5d7086268eaa321c6d19e27
joao <joaovicthorcampos@gmail.com>
2018-11-14 17:32:51 -0200

    Analise sintatica e semantica feitas no gals, falta a interface grafica

---
+++ GUI/Constants.java
@@ -0,0 +1,58 @@
+public interface Constants extends ScannerConstants, ParserConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_id = 2;
+    int t_literal = 3;
+    int t_num_int = 4;
+    int t_num_real = 5;
+    int t_TOKEN_6 = 6; //";"
+    int t_TOKEN_7 = 7; //","
+    int t_TOKEN_8 = 8; //"."
+    int t_TOKEN_9 = 9; //">"
+    int t_TOKEN_10 = 10; //"<"
+    int t_TOKEN_11 = 11; //"="
+    int t_TOKEN_12 = 12; //"("
+    int t_TOKEN_13 = 13; //")"
+    int t_TOKEN_14 = 14; //"["
+    int t_TOKEN_15 = 15; //"]"
+    int t_TOKEN_16 = 16; //"+"
+    int t_TOKEN_17 = 17; //"-"
+    int t_TOKEN_18 = 18; //"–"
+    int t_TOKEN_19 = 19; //"*"
+    int t_TOKEN_20 = 20; //"/"
+    int t_TOKEN_21 = 21; //":"
+    int t_TOKEN_22 = 22; //"}"
+    int t_TOKEN_23 = 23; //"{"
+    int t_TOKEN_24 = 24; //":="
+    int t_TOKEN_25 = 25; //".."
+    int t_TOKEN_26 = 26; //"<>"
+    int t_TOKEN_27 = 27; //"<="
+    int t_TOKEN_28 = 28; //">="
+    int t_programa = 29;
+    int t_inteiro = 30;
+    int t_real = 31;
+    int t_const = 32;
+    int t_booleano = 33;
+    int t_caracter = 34;
+    int t_cadeia = 35;
+    int t_metodo = 36;
+    int t_ref = 37;
+    int t_val = 38;
+    int t_se = 39;
+    int t_entao = 40;
+    int t_enquanto = 41;
+    int t_faca = 42;
+    int t_leia = 43;
+    int t_escreva = 44;
+    int t_retorne = 45;
+    int t_senao = 46;
+    int t_ou = 47;
+    int t_div = 48;
+    int t_e = 49;
+    int t_nao = 50;
+    int t_falso = 51;
+    int t_verdadeiro = 52;
+
+}
